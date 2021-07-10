45e09cc92b956d3aafa0efaa5f7e0e6cb77237f9
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-09 22:50:49 -0300

    C# beginner exercises

---
+++ ex1009/ex1009/Program.cs
@@ -0,0 +1,23 @@
+﻿//Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1009
+
+using System;
+using System.Globalization;
+
+namespace ex1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            double salary, sales, total;
+
+            name = Console.ReadLine();
+            salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            sales = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            total = salary + (0.15 * sales);
+
+            Console.WriteLine("TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
