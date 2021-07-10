9fb0fb441509e18c8e430c5ff2b206e2e19abfe7
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-10 20:43:00 -0300

    C# beginner exercises - Math.Abs()

---
+++ ex1013/ex1013/Program.cs
@@ -0,0 +1,24 @@
+﻿//Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1013
+
+using System;
+
+namespace ex1013
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, greaterAB, greater;
+
+            string[] numbers = Console.ReadLine().Split(' ');
+            A = int.Parse(numbers[0]);
+            B = int.Parse(numbers[1]);
+            C = int.Parse(numbers[2]);
+
+            greaterAB = (A + B + Math.Abs(A - B)) / 2;
+            greater = (greaterAB+ C + Math.Abs(greaterAB - C)) / 2;
+
+            Console.WriteLine(greater + " eh o maior");
+        }
+    }
+}
