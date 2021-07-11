8df82f303dfe4b1c90d42ff0228f2f6682294b74
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-10 21:34:36 -0300

    C# beginner exercises - Math.Sqrt() / Math.Pow()

---
+++ ex1015/ex1015/Program.cs
@@ -0,0 +1,27 @@
+﻿//Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1015
+
+using System;
+using System.Globalization;
+
+namespace ex1015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, y1, x2, y2, distance;
+
+            string[] values = Console.ReadLine().Split(' ');
+            x1 = double.Parse(values[0], CultureInfo.InvariantCulture);
+            y1 = double.Parse(values[1], CultureInfo.InvariantCulture);
+           
+            values = Console.ReadLine().Split(' ');
+            x2 = double.Parse(values[0], CultureInfo.InvariantCulture);
+            y2 = double.Parse(values[1], CultureInfo.InvariantCulture);
+
+            distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine(distance.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
