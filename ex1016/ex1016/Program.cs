227721d5942fb909c29312e28058626a7caa4fe0
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-10 22:12:50 -0300

    C# beginner exercises

---
+++ ex1016/ex1016/Program.cs
@@ -0,0 +1,20 @@
+﻿//Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1016
+
+using System;
+
+namespace ex1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distance, time;
+
+            distance = int.Parse(Console.ReadLine());
+
+            time = distance * 2;
+            
+            Console.WriteLine(time + " minutos");
+        }
+    }
+}
