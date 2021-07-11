7eb9fafafab8ad4574c5a7cd58c08c72873cfb70
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-10 21:04:30 -0300

    C# beginner exercises

---
+++ ex1014/ex1014/Program.cs
@@ -0,0 +1,23 @@
+﻿//Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1014
+
+using System;
+using System.Globalization;
+
+namespace ex1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X;
+            double Y, consumption;
+
+            X = int.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            consumption = X / Y;
+
+            Console.WriteLine(consumption.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
