454f08bea32684eea76a1198aa722f40c1952b90
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-11 21:14:43 -0300

    C# beginner exercises

---
+++ ex1017/ex1017/Program.cs
@@ -0,0 +1,24 @@
+﻿//Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1017
+
+using System;
+using System.Globalization;
+
+namespace ex1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours, speed, distance;
+            double liters;
+
+            hours = int.Parse(Console.ReadLine());
+            speed = int.Parse(Console.ReadLine());
+
+            distance = hours * speed;
+            liters = distance / 12.0;
+
+            Console.WriteLine(liters.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
