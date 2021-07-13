62dff8bd3e92dc273337e7362407da89128f6029
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-13 18:17:23 -0300

    Age in days

---
+++ ex1020/ex1020/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age, years, months, days;
+
+            age = int.Parse(Console.ReadLine());
+            years = age / 365;
+            
+            age %= 365;
+            months = age / 30;
+            
+            days = age % 30;
+                       
+            Console.WriteLine(years + " ano(s)");
+            Console.WriteLine(months + " mes(es)");
+            Console.WriteLine(days + " dia(s)");
+        }
+    }
+}
