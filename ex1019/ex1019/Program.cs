1b2015b84c855a106125be6e241d48817d203786
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-13 17:32:20 -0300

    Time conversion

---
+++ ex1019/ex1019/Program.cs
@@ -0,0 +1,25 @@
+﻿//Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1019
+
+using System;
+
+namespace ex1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, hours, minutes, seconds;
+
+            N = int.Parse(Console.ReadLine());
+            hours = N / 3600;
+            
+            N = N % 3600;
+            minutes = N / 60;
+
+            N = N % 60;
+            seconds = N;
+
+            Console.WriteLine(hours + ":" + minutes + ":" + seconds);
+        }
+    }
+}
