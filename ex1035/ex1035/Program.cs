c1c8dc7f0ec7e4298039c6473d910497d61d7580
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-13 21:49:42 -0300

    if / else

---
+++ ex1035/ex1035/Program.cs
@@ -0,0 +1,30 @@
+﻿//Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1035
+
+using System;
+
+namespace ex1035
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D;
+
+            string[] numbers = Console.ReadLine().Split(' ');
+            A = int.Parse(numbers[0]);
+            B = int.Parse(numbers[1]);
+            C = int.Parse(numbers[2]);
+            D = int.Parse(numbers[3]);
+
+            if (B > C && D > A && (C + D) > (A + B) && C > 0 && D > 0 && A % 2 == 0)
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
+                     
+        }
+    }
+}
