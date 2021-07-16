c4bc969ce1942d537835cf6b20c6bf4c40c4c80f
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-16 17:13:14 -0300

    Conditionals

---
+++ ex1046/ex1046/Program.cs
@@ -0,0 +1,32 @@
+﻿//Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1046
+
+using System;
+
+namespace ex1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start, end, total_hours = 0;
+
+            string[] hours = Console.ReadLine().Split(' ');
+            start = int.Parse(hours[0]);
+            end = int.Parse(hours[1]);
+
+            if (start == end){
+                Console.WriteLine("O JOGO DUROU 24 HORA(S)");
+            }
+            else {
+                if (start < end) {
+                    total_hours = end - start;
+                }
+                else {
+                    total_hours = (24 - start) + end;
+                }
+                Console.WriteLine("O JOGO DUROU " + total_hours + " HORA(S)");
+            }
+
+        }
+    }
+}
