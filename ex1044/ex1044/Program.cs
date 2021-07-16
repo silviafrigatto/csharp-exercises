de30990a84ff0edb1cf2d5513b9da83dc6a7b9e4
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-16 12:59:40 -0300

    Conditionals

---
+++ ex1044/ex1044/Program.cs
@@ -0,0 +1,26 @@
+﻿//Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1044
+
+using System;
+
+namespace ex1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            string[] numbers = Console.ReadLine().Split(' ');
+            a = int.Parse(numbers[0]);
+            b = int.Parse(numbers[1]);
+
+            if (a % b == 0 || b % a == 0) {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+
+        }
+    }
+}
