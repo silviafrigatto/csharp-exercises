7762135e3bae54f9e33efc36fded7a2207ebbb70
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-22 19:59:01 -0300

    For

---
+++ ex1064/ex1064/Program.cs
@@ -0,0 +1,30 @@
+﻿// Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1064
+
+using System;
+using System.Globalization;
+
+namespace ex1064
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            double num, sum = 0.0, average = 0.0;
+
+            for (int i = 1; i < 7; i++)
+            {
+                num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (num > 0)
+                {
+                    total += 1;
+                    sum += num;
+                }
+            }
+
+            average = sum / total;
+            Console.WriteLine(total + " valores positivos");
+            Console.WriteLine(average.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
