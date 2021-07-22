61c7167a6e698a604c6f062874161705e10e8cbe
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-22 15:23:58 -0300

    List

---
+++ ex1052/ex1052/Program.cs
@@ -0,0 +1,19 @@
+﻿// Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1052
+
+using System;
+
+namespace ex1052
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            num = int.Parse(Console.ReadLine());
+            string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+           
+            Console.WriteLine(months[num - 1]);
+        }
+    }
+}
