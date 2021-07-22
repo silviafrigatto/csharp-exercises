823a9ec9097f5cf204fc5ed6386cfec6359f11d7
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-22 15:32:54 -0300

    For

---
+++ ex1059/ex1059/Program.cs
@@ -0,0 +1,20 @@
+﻿// Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1059
+
+using System;
+
+namespace ex1059
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }         
+            }
+        }
+    }
+}
