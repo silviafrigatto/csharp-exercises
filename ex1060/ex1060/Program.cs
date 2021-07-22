3902f111ea4d864eae8a81abeac2ef71b27c6030
Silvia Frigatto <silvia.frigatto@gmail.com>
2021-07-22 19:25:17 -0300

    For

---
+++ ex1060/ex1060/Program.cs
@@ -0,0 +1,24 @@
+﻿// Exercise available on https://www.urionlinejudge.com.br/judge/en/problems/view/1060
+
+using System;
+using System.Globalization;
+
+namespace ex1060
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            double num;
+
+            for (int i = 1;  i < 7; i++) {
+                num = double.Parse(Console.ReadLine());
+                if (num > 0) {
+                    total += 1;
+                }
+            }
+            Console.WriteLine(total + " valores positivos");
+        }
+    }
+}
