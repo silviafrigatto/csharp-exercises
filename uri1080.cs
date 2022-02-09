cd75313e1bdab044c088f800ee4dbe8afa9da7ef
Silvia Frigatto <silvia.frigatto@gmail.com>
2022-02-08 22:30:16 -0300

    Started uri1080.cs

---
+++ uri1080.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program {
+    static void Main(string[] args) {
+        
+		// Ler um numero
+		// Adicionar o número a uma lista
+		// imprimir o maior
+		// imprimir a posição do maior
+		
+		
+		int num = int.Parse(Console.ReadLine());
+		int[] list = new int[];
+		
+		
+    }
+}
